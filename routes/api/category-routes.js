587e24957defdacd3aba9de81b38ef8e1f767de6
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-14 13:46:23 -0500

    Category Post Successful

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,15 +37,30 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try{
-    const catData = await Category.create(req.body);
-    res.status(200).json(catData);
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+}
+);
+  if (!categoryData) {
+    res.status(404).json({ message: "No Tag found with this id!" })
+    return;
+  }
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
