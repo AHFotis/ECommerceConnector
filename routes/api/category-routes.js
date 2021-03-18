973767ae29e433d5b19ca62cf9fbf96896fd89fb
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-18 09:39:51 -0500

    Code Clean

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // find all categorie
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product}],
@@ -18,7 +17,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   try{
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
